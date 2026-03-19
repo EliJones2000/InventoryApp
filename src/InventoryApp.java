01e00c758adb1922b7f3c0abc4ec708378e8d556
james <@N/A>
2026-03-19 15:55:59 -0500

    Product.java

---
+++ src/InventoryApp.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class InventoryApp {
+    public static void main(String[] args) {
+       Inventory inventory = new Inventory();
+
+       // Preload 10 products
+       inventory.addProduct
+    }
+}
--- src/Product.java
+++ src/Product.java
@@ -1,2 +1,57 @@
 public class Product {
+
+    // Fields
+    private int productId;
+    private String name;
+    private double price;
+    private int quantity;
+
+    // Constructor
+    public Product(int productId, String name, double price, int quantity){
+        this.productId = productId;
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    // Getters
+    public int getProductId() {
+        return productId;
+    }
+    public String getName() {
+        return name;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public int getQuantity() {
+        return quantity;
+    }
+
+    // Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setPrice(double price) {
+        if (price >= 0) {
+            this.price = price;
+        }
+    }
+    public void setQuantity(int quantity) {
+        if (quantity >= 0) {
+            this.quantity = quantity;
+        }
+    }
+    public  boolean isLowStock() {
+        return quantity < 5; // threshold can be changed
+    }
+
+    // Display product info
+    @Override
+    public String toString() {
+        return "ID: " + productId +
+                ", Name: " + name +
+                ", Price: $" + price +
+                ", Quantity: " + quantity;
+    }
 }
