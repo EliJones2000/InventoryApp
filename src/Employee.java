f2b63c5ba99f6dd0e713317b884d50f5d71f8192
james <@N/A>
2026-03-23 21:19:02 -0500

    Classes started

--- src/Employee.java
+++ src/Employee.java
@@ -1,2 +1,18 @@
-public class Employee {
+public class Employee extends User {
+
+    private String position;
+
+    public Employee(int userId, String username, String password, String position) {
+        super(userId, username, password);
+        this.position = position;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    @Override
+    public String getRole() {
+        return "Employee";
+    }
 }
--- src/User.java
+++ src/User.java
@@ -1,2 +1,28 @@
 public class User {
+
+    protected int userId;
+    protected String username;
+    protected String password;
+
+    public User(int userId, String username, String password) {
+        this.userId = userId;
+        this.username = username;
+        this.password = password;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public boolean authenticate(String inputPassword) {
+        return this.password.equals(inputPassword);
+    }
+
+    public String getRole() {
+        return "User";
+    }
 }
